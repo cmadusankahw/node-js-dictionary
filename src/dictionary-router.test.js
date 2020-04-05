6e6f5facbea785ecfd45d270a70a4abd17c015d8
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-04-06 00:42:08 +0530

    testing done part 2

--- src/dictionary-router.test.js
+++ src/dictionary-router.test.js
@@ -1,12 +1,13 @@
 //get express to create a fake app for testing
 const express = require ("express");
 const request = require("supertest");
-const dictionaryRouter = require ("./dictionary-router");
 
+const dictionaryRouter = require ("./dictionary-router");
 
 //mocking a fake data set as SkiTerms
 jest.mock("../data/skiTerms.json", () =>[
-    {term: "aaa", defined: "define a"}
+    {term: "aaa", defined: "define a"},
+    {term: "bbb", defined: "define b"}
 ]);
 
 
@@ -15,10 +16,22 @@ const app = express();
 app.use("/dictionary", dictionaryRouter );
 
 describe ("dictionary-router", () => {
-    it("GET dictionary/ Success", async () =>{
+    it("GET dictionary/ - Success", async () =>{
         const body = await  request(app).get("/dictionary");
         expect(body).toEqual([
-            {term: "aaa", defined: "define a"}
+            {term: "aaa", defined: "define a"},
+            {term: "bbb", defined: "define b"}
         ]);
-    })
+    });
+
+    it("DELETE dictionary/aaa - Success", async () =>{
+        const body = await  request(app).delete("/dictionary/aaa");
+        expect(body).toEqual(
+            {
+                status: "success",
+                removed: "aaa",
+                newLength: 1
+            }
+        );
+    });
 })
